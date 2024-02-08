9ea379927f068678f6ccb5bf1207eb3b1c5b2bd6
Damir Rysaev <freepad@ya.ru>
2024-02-08 17:11:37 +0300

    add sortablejs

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,19 @@
 import "../css/style.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
+import Sortable from "sortablejs";
+
+const columnsEl = document.querySelectorAll(".js-col");
+
+for (const columnEl of columnsEl) {
+  new Sortable(columnEl, {
+    group: "shared", // set both lists to same group
+    animation: 150,
+    draggable: ".card",
+    onEnd(event) {
+      // eslint-disable-next-line no-debugger
+      debugger;
+    },
+  });
+}
